b5b829e66a768f28af0abde18872237b4aee34b1
gifdog97 <omusubset.s@gmail.com>
2018-10-29 19:26:27 +0900

    arranged order (core.cpp, inst.hpp)

--- simu_source/core.cpp
+++ simu_source/core.cpp
@@ -104,72 +104,12 @@ class Core
     const uint32_t instruction_load_address = 0;
     const int default_stack_pointer = 2;
     const int default_stack_dump_size = 48;
+    const int *stat;
     Memory *m;
     Register *r;
     IO *io;
 
     Settings *settings;
-
-    void add(Decoder *d)
-    {
-        uint32_t x = r->get_ireg(d->rs1());
-        uint32_t y = r->get_ireg(d->rs2());
-        r->set_ireg(d->rd(), ALU::add(x, y));
-    }
-    void sub(Decoder *d)
-    {
-        uint32_t x = r->get_ireg(d->rs1());
-        uint32_t y = r->get_ireg(d->rs2());
-        r->set_ireg(d->rd(), ALU::sub(x, y));
-    }
-    void sll(Decoder *d)
-    {
-        uint32_t x = r->get_ireg(d->rs1());
-        uint32_t y = r->get_ireg(d->rs2());
-        r->set_ireg(d->rd(), ALU::sll(x, y));
-    }
-    void slt(Decoder *d)
-    {
-        uint32_t x = r->get_ireg(d->rs1());
-        uint32_t y = r->get_ireg(d->rs2());
-        r->set_ireg(d->rd(), ALU::slt(x, y));
-    }
-    void sltu(Decoder *d)
-    {
-        uint32_t x = r->get_ireg(d->rs1());
-        uint32_t y = r->get_ireg(d->rs2());
-        r->set_ireg(d->rd(), ALU::sltu(x, y));
-    }
-    void xor_(Decoder *d)
-    {
-        uint32_t x = r->get_ireg(d->rs1());
-        uint32_t y = r->get_ireg(d->rs2());
-        r->set_ireg(d->rd(), ALU::xor_(x, y));
-    }
-    void or_(Decoder *d)
-    {
-        uint32_t x = r->get_ireg(d->rs1());
-        uint32_t y = r->get_ireg(d->rs2());
-        r->set_ireg(d->rd(), ALU::or_(x, y));
-    }
-    void and_(Decoder *d)
-    {
-        uint32_t x = r->get_ireg(d->rs1());
-        uint32_t y = r->get_ireg(d->rs2());
-        r->set_ireg(d->rd(), ALU::and_(x, y));
-    }
-    void sra(Decoder *d)
-    {
-        uint32_t x = r->get_ireg(d->rs1());
-        uint32_t y = r->get_ireg(d->rs2());
-        r->set_ireg(d->rd(), ALU::sra(x, y));
-    }
-    void srl(Decoder *d)
-    {
-        uint32_t x = r->get_ireg(d->rs1());
-        uint32_t y = r->get_ireg(d->rs2());
-        r->set_ireg(d->rd(), ALU::srl(x, y));
-    }
 
     void lui(Decoder *d)
     {
@@ -183,12 +123,22 @@ class Core
         val += (int32_t)(r->ip);
         r->set_ireg(d->rd(), val);
     }
+
     void jal(Decoder *d)
     {
         int32_t imm = d->jal_imm();
         r->set_ireg(d->rd(), r->ip + 4);
         r->ip = (int32_t)r->ip + imm;
     }
+    void jalr(Decoder *d)
+    {
+        // sign extended
+        int32_t imm = d->i_type_imm();
+        int32_t s = r->get_ireg(d->rs1());
+        r->set_ireg(d->rd(), r->ip + 4);
+        r->ip = s + imm;
+    }
+
     void branch_inner(Decoder *d, int flag)
     {
         if (flag)
@@ -223,185 +173,6 @@ class Core
     void bgeu(Decoder *d)
     {
         branch_inner(d, r->get_ireg(d->rs1()) >= r->get_ireg(d->rs2()));
-    }
-    void jalr(Decoder *d)
-    {
-        // sign extended
-        int32_t imm = d->i_type_imm();
-        int32_t s = r->get_ireg(d->rs1());
-        r->set_ireg(d->rd(), r->ip + 4);
-        r->ip = s + imm;
-    }
-    void addi(Decoder *d)
-    {
-        uint32_t x = r->get_ireg(d->rs1());
-        uint32_t y = d->i_type_imm();
-        r->set_ireg(d->rd(), ALU::add(x, y));
-    }
-    void slti(Decoder *d)
-    {
-        uint32_t x = r->get_ireg(d->rs1());
-        uint32_t y = d->i_type_imm();
-        r->set_ireg(d->rd(), ALU::slt(x, y));
-    }
-    void srli(Decoder *d)
-    {
-        uint32_t x = r->get_ireg(d->rs1());
-        uint32_t y = d->i_type_imm();
-        r->set_ireg(d->rd(), ALU::srl(x, y));
-    }
-    void sltiu(Decoder *d)
-    {
-        uint32_t x = r->get_ireg(d->rs1());
-        uint32_t y = d->i_type_imm();
-        r->set_ireg(d->rd(), ALU::sltu(x, y));
-    }
-    void xori(Decoder *d)
-    {
-        uint32_t x = r->get_ireg(d->rs1());
-        uint32_t y = d->i_type_imm();
-        r->set_ireg(d->rd(), ALU::xor_(x, y));
-    }
-    void ori(Decoder *d)
-    {
-        uint32_t x = r->get_ireg(d->rs1());
-        uint32_t y = d->i_type_imm();
-        r->set_ireg(d->rd(), ALU::or_(x, y));
-    }
-    void andi(Decoder *d)
-    {
-        uint32_t x = r->get_ireg(d->rs1());
-        uint32_t y = d->i_type_imm();
-        r->set_ireg(d->rd(), ALU::and_(x, y));
-    }
-    void slli(Decoder *d)
-    {
-        uint32_t x = r->get_ireg(d->rs1());
-        uint32_t y = d->i_type_imm();
-        r->set_ireg(d->rd(), ALU::sll(x, y));
-    }
-
-    void sr(Decoder *d)
-    {
-        switch (static_cast<ALU_SR_Inst>(d->funct7()))
-        {
-        case ALU_SR_Inst::SRA:
-            sra(d);
-            break;
-        case ALU_SR_Inst::SRL:
-            srl(d);
-            break;
-        default:
-            error_dump("対応していないfunct7が使用されました: %x\n", d->funct7());
-        }
-    }
-
-    void add_sub(Decoder *d)
-    {
-        switch (static_cast<ALU_ADD_SUB_Inst>(d->funct7()))
-        {
-        case ALU_ADD_SUB_Inst::ADD:
-            add(d);
-            break;
-        case ALU_ADD_SUB_Inst::SUB:
-            sub(d);
-            break;
-        default:
-            error_dump("対応していないfunct7が使用されました: %x\n", d->funct7());
-        }
-    }
-
-    void alui(Decoder *d)
-    {
-        switch (static_cast<ALUI_Inst>(d->funct3()))
-        {
-        case ALUI_Inst::ADDI:
-            addi(d);
-            break;
-        case ALUI_Inst::SLTI:
-            slti(d);
-            break;
-        case ALUI_Inst::SLTIU:
-            sltiu(d);
-            break;
-        case ALUI_Inst::XORI:
-            xori(d);
-            break;
-        case ALUI_Inst::ORI:
-            ori(d);
-            break;
-        case ALUI_Inst::ANDI:
-            andi(d);
-            break;
-        case ALUI_Inst::SLLI:
-            slli(d);
-            break;
-        case ALUI_Inst::SRLI:
-            srli(d);
-            break;
-        default:
-            error_dump("対応していないfunct3が使用されました: %x\n", d->funct3());
-        }
-    }
-
-    void alu(Decoder *d)
-    {
-        switch (static_cast<ALU_Inst>(d->funct3()))
-        {
-        case ALU_Inst::ADD_SUB:
-            add_sub(d);
-            break;
-        case ALU_Inst::SLL:
-            sll(d);
-            break;
-        case ALU_Inst::SLT:
-            slt(d);
-            break;
-        case ALU_Inst::SLTU:
-            sltu(d);
-            break;
-        case ALU_Inst::XOR:
-            xor_(d);
-            break;
-        case ALU_Inst::SR:
-            sr(d);
-            break;
-        case ALU_Inst::OR:
-            or_(d);
-            break;
-        case ALU_Inst::AND:
-            and_(d);
-            break;
-        default:
-            error_dump("対応していないfunct3が使用されました: %x\n", d->funct3());
-        }
-    }
-
-    void branch(Decoder *d)
-    {
-        switch (static_cast<Branch_Inst>(d->funct3()))
-        {
-        case Branch_Inst::BEQ:
-            beq(d);
-            break;
-        case Branch_Inst::BNE:
-            bne(d);
-            break;
-        case Branch_Inst::BLT:
-            blt(d);
-            break;
-        case Branch_Inst::BGE:
-            bge(d);
-            break;
-        case Branch_Inst::BLTU:
-            bltu(d);
-            break;
-        case Branch_Inst::BGEU:
-            bgeu(d);
-            break;
-        default:
-            error_dump("対応していないfunct3が使用されました: %x\n", d->funct3());
-        }
     }
 
     void lb(Decoder *d)
@@ -451,32 +222,6 @@ class Core
         r->set_ireg(d->rd(), val);
     }
 
-    void load(Decoder *d)
-    {
-        switch (static_cast<Load_Inst>(d->funct3()))
-        {
-        case Load_Inst::LB:
-            lb(d);
-            break;
-        case Load_Inst::LH:
-            lh(d);
-            break;
-        case Load_Inst::LW:
-            lw(d);
-            break;
-        case Load_Inst::LBU:
-            lbu(d);
-            break;
-        case Load_Inst::LHU:
-            lhu(d);
-            break;
-        default:
-            error_dump("対応していないfunct3が使用されました: %x\n", d->funct3());
-            r->ip += 4;
-            break;
-        }
-    }
-
     void sb(Decoder *d)
     {
         uint32_t base = r->get_ireg(d->rs1());
@@ -514,6 +259,170 @@ class Core
         m->write_mem(addr, src);
     }
 
+    void addi(Decoder *d)
+    {
+        uint32_t x = r->get_ireg(d->rs1());
+        uint32_t y = d->i_type_imm();
+        r->set_ireg(d->rd(), ALU::add(x, y));
+    }
+    void slti(Decoder *d)
+    {
+        uint32_t x = r->get_ireg(d->rs1());
+        uint32_t y = d->i_type_imm();
+        r->set_ireg(d->rd(), ALU::slt(x, y));
+    }
+    void sltiu(Decoder *d)
+    {
+        uint32_t x = r->get_ireg(d->rs1());
+        uint32_t y = d->i_type_imm();
+        r->set_ireg(d->rd(), ALU::sltu(x, y));
+    }
+    void xori(Decoder *d)
+    {
+        uint32_t x = r->get_ireg(d->rs1());
+        uint32_t y = d->i_type_imm();
+        r->set_ireg(d->rd(), ALU::xor_(x, y));
+    }
+    void ori(Decoder *d)
+    {
+        uint32_t x = r->get_ireg(d->rs1());
+        uint32_t y = d->i_type_imm();
+        r->set_ireg(d->rd(), ALU::or_(x, y));
+    }
+    void andi(Decoder *d)
+    {
+        uint32_t x = r->get_ireg(d->rs1());
+        uint32_t y = d->i_type_imm();
+        r->set_ireg(d->rd(), ALU::and_(x, y));
+    }
+    void slli(Decoder *d)
+    {
+        uint32_t x = r->get_ireg(d->rs1());
+        uint32_t y = d->i_type_imm();
+        r->set_ireg(d->rd(), ALU::sll(x, y));
+    }
+    void srli(Decoder *d)
+    {
+        uint32_t x = r->get_ireg(d->rs1());
+        uint32_t y = d->i_type_imm();
+        r->set_ireg(d->rd(), ALU::srl(x, y));
+    }
+    // void srai 
+    
+    void add(Decoder *d)
+    {
+        uint32_t x = r->get_ireg(d->rs1());
+        uint32_t y = r->get_ireg(d->rs2());
+        r->set_ireg(d->rd(), ALU::add(x, y));
+    }
+    void sub(Decoder *d)
+    {
+        uint32_t x = r->get_ireg(d->rs1());
+        uint32_t y = r->get_ireg(d->rs2());
+        r->set_ireg(d->rd(), ALU::sub(x, y));
+    }
+    void sll(Decoder *d)
+    {
+        uint32_t x = r->get_ireg(d->rs1());
+        uint32_t y = r->get_ireg(d->rs2());
+        r->set_ireg(d->rd(), ALU::sll(x, y));
+    }
+    void slt(Decoder *d)
+    {
+        uint32_t x = r->get_ireg(d->rs1());
+        uint32_t y = r->get_ireg(d->rs2());
+        r->set_ireg(d->rd(), ALU::slt(x, y));
+    }
+    void sltu(Decoder *d)
+    {
+        uint32_t x = r->get_ireg(d->rs1());
+        uint32_t y = r->get_ireg(d->rs2());
+        r->set_ireg(d->rd(), ALU::sltu(x, y));
+    }
+    void xor_(Decoder *d)
+    {
+        uint32_t x = r->get_ireg(d->rs1());
+        uint32_t y = r->get_ireg(d->rs2());
+        r->set_ireg(d->rd(), ALU::xor_(x, y));
+    }
+    void srl(Decoder *d)
+    {
+        uint32_t x = r->get_ireg(d->rs1());
+        uint32_t y = r->get_ireg(d->rs2());
+        r->set_ireg(d->rd(), ALU::srl(x, y));
+    }
+    void sra(Decoder *d)
+    {
+        uint32_t x = r->get_ireg(d->rs1());
+        uint32_t y = r->get_ireg(d->rs2());
+        r->set_ireg(d->rd(), ALU::sra(x, y));
+    }
+    void or_(Decoder *d)
+    {
+        uint32_t x = r->get_ireg(d->rs1());
+        uint32_t y = r->get_ireg(d->rs2());
+        r->set_ireg(d->rd(), ALU::or_(x, y));
+    }
+    void and_(Decoder *d)
+    {
+        uint32_t x = r->get_ireg(d->rs1());
+        uint32_t y = r->get_ireg(d->rs2());
+        r->set_ireg(d->rd(), ALU::and_(x, y));
+    }
+
+    void branch(Decoder *d)
+    {
+        switch (static_cast<Branch_Inst>(d->funct3()))
+        {
+        case Branch_Inst::BEQ:
+            beq(d);
+            break;
+        case Branch_Inst::BNE:
+            bne(d);
+            break;
+        case Branch_Inst::BLT:
+            blt(d);
+            break;
+        case Branch_Inst::BGE:
+            bge(d);
+            break;
+        case Branch_Inst::BLTU:
+            bltu(d);
+            break;
+        case Branch_Inst::BGEU:
+            bgeu(d);
+            break;
+        default:
+            error_dump("対応していないfunct3が使用されました: %x\n", d->funct3());
+        }
+    }
+
+    void load(Decoder *d)
+    {
+        switch (static_cast<Load_Inst>(d->funct3()))
+        {
+        case Load_Inst::LB:
+            lb(d);
+            break;
+        case Load_Inst::LH:
+            lh(d);
+            break;
+        case Load_Inst::LW:
+            lw(d);
+            break;
+        case Load_Inst::LBU:
+            lbu(d);
+            break;
+        case Load_Inst::LHU:
+            lhu(d);
+            break;
+        default:
+            error_dump("対応していないfunct3が使用されました: %x\n", d->funct3());
+            r->ip += 4;
+            break;
+        }
+    }
+
     void store(Decoder *d)
     {
         switch (static_cast<Store_Inst>(d->funct3()))
@@ -534,6 +443,104 @@ class Core
         }
     }
 
+    void alui(Decoder *d)
+    {
+        switch (static_cast<ALUI_Inst>(d->funct3()))
+        {
+        case ALUI_Inst::ADDI:
+            addi(d);
+            break;
+        case ALUI_Inst::SLTI:
+            slti(d);
+            break;
+        case ALUI_Inst::SLTIU:
+            sltiu(d);
+            break;
+        case ALUI_Inst::XORI:
+            xori(d);
+            break;
+        case ALUI_Inst::ORI:
+            ori(d);
+            break;
+        case ALUI_Inst::ANDI:
+            andi(d);
+            break;
+        case ALUI_Inst::SLLI:
+            slli(d);
+            break;
+        case ALUI_Inst::SRLI:
+            srli(d);
+            break;
+        default:
+            error_dump("対応していないfunct3が使用されました: %x\n", d->funct3());
+        }
+    }
+
+    void add_sub(Decoder *d)
+    {
+        switch (static_cast<ALU_ADD_SUB_Inst>(d->funct7()))
+        {
+        case ALU_ADD_SUB_Inst::ADD:
+            add(d);
+            break;
+        case ALU_ADD_SUB_Inst::SUB:
+            sub(d);
+            break;
+        default:
+            error_dump("対応していないfunct7が使用されました: %x\n", d->funct7());
+        }
+    }
+
+    void sr(Decoder *d)
+    {
+        switch (static_cast<ALU_SR_Inst>(d->funct7()))
+        {
+        case ALU_SR_Inst::SRA:
+            sra(d);
+            break;
+        case ALU_SR_Inst::SRL:
+            srl(d);
+            break;
+        default:
+            error_dump("対応していないfunct7が使用されました: %x\n", d->funct7());
+        }
+    }
+
+    void alu(Decoder *d)
+    {
+        switch (static_cast<ALU_Inst>(d->funct3()))
+        {
+        case ALU_Inst::ADD_SUB:
+            add_sub(d);
+            break;
+        case ALU_Inst::SLL:
+            sll(d);
+            break;
+        case ALU_Inst::SLT:
+            slt(d);
+            break;
+        case ALU_Inst::SLTU:
+            sltu(d);
+            break;
+        case ALU_Inst::XOR:
+            xor_(d);
+            break;
+        case ALU_Inst::SR:
+            sr(d);
+            break;
+        case ALU_Inst::OR:
+            or_(d);
+            break;
+        case ALU_Inst::AND:
+            and_(d);
+            break;
+        default:
+            error_dump("対応していないfunct3が使用されました: %x\n", d->funct3());
+        }
+    }
+
+
+
     void flw(Decoder *d)
     {
         uint32_t base = r->get_ireg(d->rs1());
@@ -543,20 +550,6 @@ class Core
         uint32_t addr = base + offset;
         uint32_t val = m->read_mem_4(addr);
         r->set_freg_raw(d->rd(), val);
-    }
-
-    void fload(Decoder *d)
-    {
-        switch (static_cast<FLoad_Inst>(d->funct3()))
-        {
-        case FLoad_Inst::FLW:
-            flw(d);
-            break;
-        default:
-            error_dump("widthがおかしいです(仕様書p112): %x\n", d->funct3());
-            r->ip += 4;
-            break;
-        }
     }
 
     void fsw(Decoder *d)
@@ -570,20 +563,6 @@ class Core
         m->write_mem(addr, src);
     }
 
-    void fstore(Decoder *d)
-    {
-        switch (static_cast<FStore_Inst>(d->funct3()))
-        {
-        case FStore_Inst::FSW:
-            fsw(d);
-            break;
-        default:
-            error_dump("widthがおかしいです(仕様書p112): %x\n", d->funct3());
-            r->ip += 4;
-            break;
-        }
-    }
-
     void fadd(Decoder *d)
     {
         if(d->rm() != 0){
@@ -593,7 +572,6 @@ class Core
         float y = r->get_freg(d->rs2());
         r->set_freg(d->rd(), FPU::fadd(x, y));
     }
-
     void fsub(Decoder *d)
     {
         if(d->rm() != 0){
@@ -603,7 +581,6 @@ class Core
         float y = r->get_freg(d->rs2());
         r->set_freg(d->rd(), FPU::fsub(x, y));
     }
-
     void fmul(Decoder *d)
     {
         if(d->rm() != 0){
@@ -613,7 +590,6 @@ class Core
         float y = r->get_freg(d->rs2());
         r->set_freg(d->rd(), FPU::fmul(x, y));
     }
-
     void fdiv(Decoder *d)
     {
         if(d->rm() != 0){
@@ -623,7 +599,6 @@ class Core
         float y = r->get_freg(d->rs2());
         r->set_freg(d->rd(), FPU::fdiv(x, y));
     }
-
     void fsqrt(Decoder *d)
     {
         if(d->rm() != 0){
@@ -634,6 +609,19 @@ class Core
         }
         float x = r->get_freg(d->rs1());
         r->set_freg(d->rd(), FPU::fsqrt(x));
+    }
+
+    void _fsgnj(Decoder *d) 
+    {
+        float x = r->get_freg(d->rs1());
+        float y = r->get_freg(d->rs2());
+        r->set_freg(d->rd(), x * y > 0 ? x : -x);
+    }
+    void fsgnjn(Decoder *d) 
+    {
+        float x = r->get_freg(d->rs1());
+        float y = r->get_freg(d->rs2());
+        r->set_freg(d->rd(), x * y > 0 ? -x : x);
     }
 
     void fcvt_w_s(Decoder *d)
@@ -647,7 +635,6 @@ class Core
         float x = r->get_freg(d->rs1());
         r->set_ireg(d->rd(), FPU::float2int(x));
     }
-
     void fcvt_s_w(Decoder *d)
     {
         if(d->rm() != 0){
@@ -679,6 +666,50 @@ class Core
         r->set_ireg(d->rd(),FPU::fle(x,y));
     }
 
+    void fload(Decoder *d)
+    {
+        switch (static_cast<FLoad_Inst>(d->funct3()))
+        {
+        case FLoad_Inst::FLW:
+            flw(d);
+            break;
+        default:
+            error_dump("widthがおかしいです(仕様書p112): %x\n", d->funct3());
+            r->ip += 4;
+            break;
+        }
+    }
+
+    void fstore(Decoder *d)
+    {
+        switch (static_cast<FStore_Inst>(d->funct3()))
+        {
+        case FStore_Inst::FSW:
+            fsw(d);
+            break;
+        default:
+            error_dump("widthがおかしいです(仕様書p112): %x\n", d->funct3());
+            r->ip += 4;
+            break;
+        }
+    }
+
+    void fsgnj(Decoder *d) 
+    {
+        switch (static_cast<FSgnj_Inst>(d->funct3())) 
+        {
+        case FSgnj_Inst::FSGNJ:
+            _fsgnj(d);
+            break;
+        case FSgnj_Inst::FSGNJN:
+            fsgnjn(d);
+            break;
+        case FSgnj_Inst::FSGNJX:
+        default:
+            error_dump("対応していないfunct3が使用されました: %x\n", d->funct3());
+        }
+    }
+
     void fcomp(Decoder *d)
     {
         switch (static_cast<FComp_Inst>(d->funct3()))
@@ -692,36 +723,6 @@ class Core
         case FComp_Inst::FLE:
             fle(d);
             break;
-        default:
-            error_dump("対応していないfunct3が使用されました: %x\n", d->funct3());
-        }
-    }
-
-    void _fsgnj(Decoder *d) 
-    {
-        float x = r->get_freg(d->rs1());
-        float y = r->get_freg(d->rs2());
-        r->set_freg(d->rd(), x * y > 0 ? x : -x);
-    }
-
-    void fsgnjn(Decoder *d) 
-    {
-        float x = r->get_freg(d->rs1());
-        float y = r->get_freg(d->rs2());
-        r->set_freg(d->rd(), x * y > 0 ? -x : x);
-    }
-
-    void fsgnj(Decoder *d) 
-    {
-        switch (static_cast<FSGNJ_Inst>(d->funct3())) 
-        {
-        case FSGNJ_Inst::FSGNJ:
-            _fsgnj(d);
-            break;
-        case FSGNJ_Inst::FSGNJN:
-            fsgnjn(d);
-            break;
-        case FSGNJ_Inst::FSGNJX:
         default:
             error_dump("対応していないfunct3が使用されました: %x\n", d->funct3());
         }
--- simu_source/inst.hpp
+++ simu_source/inst.hpp
@@ -8,12 +8,35 @@ enum struct Inst : uint8_t {
     STORE   = 0b0100011,
     ALUI    = 0b0010011,
     ALU     = 0b0110011,
-    FENCE   = 0b0001111,
-    ECALL   = 0b1110011,
-    EBREAK  = 0b1110011,
+    FENCE   = 0b0001111, // not yet implemented
+    ECALL   = 0b1110011, // not yet implemented
+    EBREAK  = 0b1110011, // not yet implemented
     FLOAD   = 0b0000111,
     FSTORE  = 0b0100111,
     FPU     = 0b1010011,
+};
+
+enum struct Branch_Inst : uint8_t {
+    BEQ     = 0b000,
+    BNE     = 0b001,
+    BLT     = 0b100,
+    BGE     = 0b101,
+    BLTU    = 0b110,
+    BGEU    = 0b111,
+};
+
+enum struct Load_Inst : uint8_t {
+    LB  = 0b000,
+    LH  = 0b001,
+    LW  = 0b010,
+    LBU = 0b100,
+    LHU = 0b101,
+};
+
+enum struct Store_Inst : uint8_t {
+    SB = 0b000,
+    SH = 0b001,
+    SW = 0b010
 };
 
 enum struct ALUI_Inst : uint8_t{
@@ -32,6 +55,16 @@ enum struct ALUI_SRL_Inst : uint8_t {
     SRAI = 0b0100000,
 };
 
+enum struct ALU_ADD_SUB_Inst : uint8_t {
+    ADD = 0b0000000,
+    SUB = 0b0100000,
+};
+
+enum struct ALU_SR_Inst : uint8_t {
+    SRL = 0b0000000,
+    SRA = 0b0100000,
+};
+
 enum struct ALU_Inst : uint8_t{
     ADD_SUB = 0b000,
     SLL     = 0b001,
@@ -41,6 +74,32 @@ enum struct ALU_Inst : uint8_t{
     SR      = 0b101,
     OR      = 0b110,
     AND     = 0b111,
+};
+
+//not yet implemented
+enum struct Fence_Inst : uint8_t {
+    FENCE   = 0b000,
+    FENCEI  = 0b001,
+};
+
+enum struct FLoad_Inst : uint8_t{
+    FLW = 0b010,
+};
+
+enum struct FStore_Inst : uint8_t{
+    FSW = 0b010,
+};
+
+enum struct FSgnj_Inst : uint8_t {
+    FSGNJ = 0b000,
+    FSGNJN = 0b001,
+    FSGNJX = 0b010
+};
+
+enum struct FComp_Inst : uint8_t{
+    FEQ = 0b010,
+    FLT = 0b001,
+    FLE = 0b000,
 };
 
 enum struct FPU_Inst : uint8_t{
@@ -57,60 +116,3 @@ enum struct FPU_Inst : uint8_t{
     FSGNJ = 0b0010000,
 };
 
-enum struct FLoad_Inst : uint8_t{
-    FLW = 0b010,
-};
-
-enum struct FStore_Inst : uint8_t{
-    FSW = 0b010,
-};
-
-enum struct FComp_Inst : uint8_t{
-    FEQ = 0b010,
-    FLT = 0b001,
-    FLE = 0b000,
-};
-
-enum struct ALU_ADD_SUB_Inst : uint8_t {
-    ADD = 0b0000000,
-    SUB = 0b0100000,
-};
-
-enum struct ALU_SR_Inst : uint8_t {
-    SRL = 0b0000000,
-    SRA = 0b0100000,
-};
-
-enum struct Branch_Inst : uint8_t {
-    BEQ     = 0b000,
-    BNE     = 0b001,
-    BLT     = 0b100,
-    BGE     = 0b101,
-    BLTU    = 0b110,
-    BGEU    = 0b111,
-};
-
-enum struct Fence_Inst : uint8_t {
-    FENCE   = 0b000,
-    FENCEI  = 0b001,
-};
-
-enum struct Load_Inst : uint8_t {
-    LB  = 0b000,
-    LH  = 0b001,
-    LW  = 0b010,
-    LBU = 0b100,
-    LHU = 0b101,
-};
-
-enum struct Store_Inst : uint8_t {
-    SB = 0b000,
-    SH = 0b001,
-    SW = 0b010
-};
-
-enum struct FSGNJ_Inst : uint8_t {
-    FSGNJ = 0b000,
-    FSGNJN = 0b001,
-    FSGNJX = 0b010
-};
